52c1008a57eef9ad9431b4c448fe3a048b2687db
iamvenu13342 <121122983+iamvenu13342@users.noreply.github.com>
2024-06-20 18:47:27 +0100

    Create main.tf

---
+++ TERRAFORM/Infrastructure-Monitoring-Setup/modules/security_groups/main.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "monitoring_sg" {
+  name        = "monitoring_sg"
+  description = "Allow HTTP and HTTPS traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
