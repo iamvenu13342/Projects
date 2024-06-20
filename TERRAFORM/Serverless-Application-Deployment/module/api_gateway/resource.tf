f9080c257bb08b2b6a842c2885f26b783d449dd4
iamvenu13342 <121122983+iamvenu13342@users.noreply.github.com>
2024-06-20 20:02:34 +0100

    Create resource.tf

---
+++ TERRAFORM/Serverless-Application-Deployment/module/api_gateway/resource.tf
@@ -0,0 +1,9 @@
+resource "aws_api_gateway_resource" "my_resource" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  parent_id   = aws_api_gateway_rest_api.my_api.root_resource_id
+  path_part   = "resource"
+}
+
+output "api_gateway_resource_id" {
+  value = aws_api_gateway_resource.my_resource.id
+}
