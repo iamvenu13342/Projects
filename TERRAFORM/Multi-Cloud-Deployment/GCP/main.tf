94478ac3dc8030f761af52f0e4892f691c3bd7a8
iamvenu13342 <121122983+iamvenu13342@users.noreply.github.com>
2024-06-20 16:02:47 +0100

    Create main.tf

---
+++ TERRAFORM/Multi-Cloud-Deployment/GCP/main.tf
@@ -0,0 +1,15 @@
+resource "google_compute_instance" "vm_instance" {
+  name         = "example-instance"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+}
