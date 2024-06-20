1477c0e1e7b6d7fc2f89ab4eee97ed00a827aac3
iamvenu13342 <121122983+iamvenu13342@users.noreply.github.com>
2024-06-20 20:02:59 +0100

    Create method.tf

---
+++ TERRAFORM/Serverless-Application-Deployment/module/api_gateway/method.tf
@@ -0,0 +1,6 @@
+resource "aws_api_gateway_method" "my_method" {
+  rest_api_id   = aws_api_gateway_rest_api.my_api.id
+  resource_id   = aws_api_gateway_resource.my_resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
