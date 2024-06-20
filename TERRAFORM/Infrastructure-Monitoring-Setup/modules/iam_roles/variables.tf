09817c123b36ad812ee7135599e7c35490d17445
iamvenu13342 <121122983+iamvenu13342@users.noreply.github.com>
2024-06-20 18:50:25 +0100

    Create variables.tf

---
+++ TERRAFORM/Infrastructure-Monitoring-Setup/modules/iam_roles/variables.tf
@@ -0,0 +1,9 @@
+variable "role_name" {
+  description = "The name of the IAM role"
+  type        = string
+}
+
+variable "policy_arn" {
+  description = "The ARN of the IAM policy to attach"
+  type        = string
+}
