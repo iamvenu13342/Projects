8cf7162b5646f7d4064bbc971a9356087c36ab09
iamvenu13342 <121122983+iamvenu13342@users.noreply.github.com>
2024-06-20 19:24:41 +0100

    Create variables.tf

---
+++ TERRAFORM/IAM-configuration/modules/iam/variables.tf
@@ -0,0 +1,9 @@
+variable "iam_user_name" {
+  description = "The name of the IAM user"
+  type        = string
+}
+
+variable "s3_bucket_name" {
+  description = "The name of the S3 bucket for policies"
+  type        = string
+}
