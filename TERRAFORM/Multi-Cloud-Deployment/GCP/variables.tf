3b1840dc16f529ac40b423789c9a7e2e62717e9d
iamvenu13342 <121122983+iamvenu13342@users.noreply.github.com>
2024-06-20 16:04:50 +0100

    Create variables.tf

---
+++ TERRAFORM/Multi-Cloud-Deployment/GCP/variables.tf
@@ -0,0 +1,9 @@
+variable "project" {
+  description = "The GCP project ID"
+  default     = "my-gcp-project"
+}
+
+variable "region" {
+  description = "The GCP region to deploy resources in"
+  default     = "us-central1"
+}
