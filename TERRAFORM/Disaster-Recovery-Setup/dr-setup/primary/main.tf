c98f1cdde39339ceff0f2aa7b65789b2a87ebc19
iamvenu13342 <121122983+iamvenu13342@users.noreply.github.com>
2024-06-21 09:22:41 +0100

    Create main.tf

---
+++ TERRAFORM/Disaster-Recovery-Setup/dr-setup/primary/main.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "web" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.main.id
+  tags = {
+    Name = "Primary-Web"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "Primary-VPC"
+  }
+}
