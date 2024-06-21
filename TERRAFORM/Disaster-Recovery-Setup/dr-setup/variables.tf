3a6d739db6a1656f47aeb9855676db5c7420ed23
iamvenu13342 <121122983+iamvenu13342@users.noreply.github.com>
2024-06-21 09:21:43 +0100

    Create variables.tf

---
+++ TERRAFORM/Disaster-Recovery-Setup/dr-setup/variables.tf
@@ -0,0 +1,11 @@
+variable "primary_region" {
+  description = "Primary AWS region"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "secondary_region" {
+  description = "Secondary AWS region"
+  type        = string
+  default     = "us-east-1"
+}
