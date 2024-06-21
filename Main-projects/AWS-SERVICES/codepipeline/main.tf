34aaa12b6ce7d4be3cba17e9817f397f9af8af94
Venu Gopal Reddy Annapureddy <121122983+iamvenu13342@users.noreply.github.com>
2024-06-21 16:37:36 +0100

    Create main.tf

---
+++ Main-projects/AWS-SERVICES/codepipeline/main.tf
@@ -0,0 +1,64 @@
+resource "aws_codepipeline" "pipeline" {
+  name = "MyPipeline"
+
+  role_arn = aws_iam_role.codepipeline.arn
+
+  artifact_store {
+    location = aws_s3_bucket.artifact.bucket
+    type     = "S3"
+  }
+
+  stage {
+    name = "Source"
+
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "AWS"
+      provider         = "CodeCommit"
+      version          = "1"
+      output_artifacts = ["SourceOutput"]
+
+      configuration = {
+        RepositoryName = aws_codecommit_repository.repo.name
+        BranchName     = "main"
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+
+    action {
+      name             = "Build"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      version          = "1"
+      input_artifacts  = ["SourceOutput"]
+      output_artifacts = ["BuildOutput"]
+
+      configuration = {
+        ProjectName = aws_codebuild_project.project.name
+      }
+    }
+  }
+
+  stage {
+    name = "Deploy"
+
+    action {
+      name            = "Deploy"
+      category        = "Deploy"
+      owner           = "AWS"
+      provider        = "CodeDeploy"
+      version         = "1"
+      input_artifacts = ["BuildOutput"]
+
+      configuration = {
+        ApplicationName     = aws_codedeploy_app.app.name
+        DeploymentGroupName = aws_codedeploy_deployment_group.group.name
+      }
+    }
+  }
+}
