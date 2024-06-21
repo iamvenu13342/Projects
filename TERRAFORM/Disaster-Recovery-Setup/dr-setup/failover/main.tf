4d84892e280028145effd2ab478d2dd7a00e32dd
iamvenu13342 <121122983+iamvenu13342@users.noreply.github.com>
2024-06-21 09:27:49 +0100

    Create main.tf

---
+++ TERRAFORM/Disaster-Recovery-Setup/dr-setup/failover/main.tf
@@ -0,0 +1,41 @@
+resource "aws_route53_record" "primary" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "example.com"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.primary.dns_name
+    zone_id                = aws_lb.primary.zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "secondary" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "example.com"
+  type    = "A"
+  set_identifier = "Secondary"
+  weight         = 10
+
+  alias {
+    name                   = aws_lb.secondary.dns_name
+    zone_id                = aws_lb.secondary.zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_lb" "primary" {
+  name               = "primary-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb.id]
+  subnets            = [aws_subnet.main.id]
+}
+
+resource "aws_lb" "secondary" {
+  name               = "secondary-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb.id]
+  subnets            = [aws_subnet.main.id]
+}
