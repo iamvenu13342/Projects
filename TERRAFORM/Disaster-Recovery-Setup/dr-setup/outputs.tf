6a3ce494379f4c8222dd1cb4eac635ef975741d6
iamvenu13342 <121122983+iamvenu13342@users.noreply.github.com>
2024-06-21 09:22:07 +0100

    Create outputs.tf

---
+++ TERRAFORM/Disaster-Recovery-Setup/dr-setup/outputs.tf
@@ -0,0 +1,15 @@
+output "primary_outputs" {
+  value = module.primary
+}
+
+output "secondary_outputs" {
+  value = module.secondary
+}
+
+output "data_replication_outputs" {
+  value = module.data_replication
+}
+
+output "failover_outputs" {
+  value = module.failover
+}
