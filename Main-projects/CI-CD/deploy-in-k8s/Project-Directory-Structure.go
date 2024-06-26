c838892540bdbefe6006688918a13383e1471343
Venu Gopal Reddy Annapureddy <121122983+iamvenu13342@users.noreply.github.com>
2024-06-26 09:03:08 +0100

    Create Project-Directory-Structure.go

---
+++ Main-projects/CI-CD/deploy-in-k8s/Project-Directory-Structure.go
@@ -0,0 +1,11 @@
+your-repo/
+├── app.js
+├── Dockerfile
+├── .dockerignore
+├── Jenkinsfile
+├── package.json
+├── package-lock.json
+├── README.md
+└── k8s/
+    ├── deployment.yaml
+    └── service.yaml
